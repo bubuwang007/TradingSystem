cfe91ce3062c8f3d9886f39e3f463e939ff02c0a
bubuwang007 <1191651771@qq.com>
2025-11-26 20:28:20 +0800

    2025.11.26

--- codes/test.py
+++ codes/test.py
@@ -4,5 +4,5 @@ from ashare.tools import get_l3_companies_info
 ts.set_token("3be1c73e47c58ec998059aa2cfe64c87e03485fba0ab10c5788372f7")
 
 with open("test_output.txt", "w", encoding="utf-8") as f:
-    for i in get_l3_companies_info("850191.SI"):
+    for i in get_l3_companies_info("851333.SI"):
         f.write(i + "\n")
--- docs/我的交易/README.md
+++ docs/我的交易/README.md
@@ -6,3 +6,5 @@ permalink: /mt/ap7mgvcu/
 # 我的交易
 
 [交易纪律](交易纪律.md)
+
+[重点关注](重点关注.md)
