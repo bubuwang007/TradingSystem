d8ef152f49e24b32ba47ab299901f6784c36707c
bubuwang007 <1191651771@qq.com>
2025-12-02 10:08:42 +0800

    hys 2025.12.2

--- codes/test.py
+++ codes/test.py
@@ -5,11 +5,5 @@ e2t = Excel2MdTable(
     "持仓",
 )
 
-<<<<<<< HEAD
-with open("test_output.txt", "w", encoding="utf-8") as f:
-    for i in get_l3_companies_info("850137.SI"):
-        f.write(i + "\n")
-=======
 res = e2t.generate()
 print(res)
->>>>>>> 8386298ef83590e5030feba8db98fe648f475b91
