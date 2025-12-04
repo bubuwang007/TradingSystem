4cc6a4a2c471a4851f2d2606defbf2791270f5eb
bubuwang007 <1191651771@qq.com>
2025-12-04 17:02:51 +0800

    hys 2025.12.4

--- codes/test.py
+++ codes/test.py
@@ -1,15 +1,4 @@
 from stock_databases import StockBasic
 
-<<<<<<< HEAD
-e2t = Excel2MdTable(
-    r"E:\股票\持仓记录\12月持仓记录.xlsx",
-    "持仓",
-    sheet_name="12.02"
-)
-
-res = e2t.generate()
-print(res)
-=======
 df = StockBasic.load_stock_basic_dataframe()
 print(df.head())
->>>>>>> 61228578e79ca6932f307cedcc6cc17795273bc7
--- codes/获取申万行业公司.py
+++ codes/获取申万行业公司.py
@@ -1,5 +1,5 @@
 from mtools import get_l3_companies_info
 
 with open("test_output.txt", "w", encoding="utf-8") as f:
-    for i in get_l3_companies_info("850832.SI"):
+    for i in get_l3_companies_info("851242.SI"):
         f.write(i + "\n")
