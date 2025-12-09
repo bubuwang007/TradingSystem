c50fbde86723826acd81582ab7f8a34e912099ca
bubuwang007 <1191651771@qq.com>
2025-12-09 21:59:08 +0800

    hys 2025.12.9

--- codes/Excel生成md.py
+++ codes/Excel生成md.py
@@ -3,7 +3,7 @@ from mtools import Excel2MdTable
 e2t = Excel2MdTable(
     r"C:\Users\hys\Desktop\creation\TradingSystem\codes\excel\hys持仓.xlsx",
     "持仓",
-    sheet_name="2025_12_2"
+    sheet_name="2025_12_9"
 )
 
 res = e2t.generate()
